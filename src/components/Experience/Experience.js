0f13ec5f9e356afcdbd4c8cdc64d0f9fc7cb4720
TorinGitIBM <torinmccallum@gmail.com>
2023-04-18 11:28:44 -0400

    add experience section

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,10 +1,38 @@
 import "./Experience.css";
+import { experience } from "../../data/data";
 
 function Experience() {
   return (
     <section className="experience" id="experience">
-      <div>
+      <div className="contact-title">
         <h2>Experience</h2>
+      </div>
+      <div className="jobs">
+        <div>
+          {experience.map((job) => {
+            const {
+              key,
+              position,
+              employer,
+              type,
+              duration,
+              description,
+              image,
+            } = job;
+
+            return (
+              <div className="job-container" key={key}>
+                <img src={image} alt={employer} />
+                <div className="job-info">
+                  <h2>{position}</h2>
+                  <p>{duration}</p>
+                  <p>{type}</p>
+                  <p>{description}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,10 @@
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import { ImProfile } from "react-icons/im";
+import handle from "../images/Handle.png";
+import website from "../images/Website.png";
+import comingSoon from "../images/ComingSoon.png";
+import lcs from "../images/LCS.jfif";
+import ibm from "../images/IBM.jfif";
 
 const links = [
   {
@@ -31,24 +36,46 @@ const projects = [
     title: "Handle: The ASL Word Guessing Game",
     description:
       "My spin on the popular puzzle game Wordle, Handle takes in detected Sing Language letters as input using MediaPipe Hands",
-    image: "Handle.png",
+    image: handle,
     link: "https://github.com/Torin99/Handle",
   },
   {
     key: 2,
     title: "Personal Website",
     description:
-      "Personal portfolio website made in ReactJS. The Site your on right now!",
-    image: "Website.png",
+      "Personal portfolio website made in ReactJS. The Site you're on right now!",
+    image: website,
     link: "https://github.com/Torin99/My-Website",
   },
   {
     key: 3,
     title: "Working On It...",
     description: "Hey no peeking! This one's not done yet. Check back soon!",
-    image: "ComingSoon.png",
+    image: comingSoon,
     link: "https://github.com/Torin99",
   },
 ];
+const experience = [
+  {
+    key: 1,
+    position: "Technical Account Associate",
+    employer: "IBM Canada",
+    type: "Internship",
+    duration: "April 2022 - Present",
+    description:
+      "Assisted TAM's with client accounts using IBM technology while also designing new tools in React and Plotly to simplify the TAM and Seller workflows",
+    image: ibm,
+  },
+  {
+    key: 2,
+    position: "Event Coordinator",
+    employer: "Laurier Computing Society",
+    type: "School Club",
+    duration: "September 2022 - Present",
+    description:
+      "Planned, organized, and presented over x events for the WLU's Fossa Club of the Year",
+    image: lcs,
+  },
+];
 
-export { links, projects };
+export { links, projects, experience };
